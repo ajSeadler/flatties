42d83c6b59d2ca7d0279e7f97aa247071801553a
ajSeadler <aj6287@gmail.com>
2025-01-30 10:15:57 -0600

    lk

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
-    outDir: "../dist", // Ensure Netlify deploys the correct folder
+    outDir: "dist", // Ensure Netlify deploys the correct folder
   },
 });
